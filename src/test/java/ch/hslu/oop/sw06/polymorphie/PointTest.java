d7374f017aab1da18f5b39cba16a840cfc9d27d8
domebuehler <domebuehler7@gmail.com>
2022-04-03 20:23:50 +0200

    cleaned

--- src/test/java/ch/hslu/oop/sw06/polymorphie/PointTest.java
+++ src/test/java/ch/hslu/oop/sw06/polymorphie/PointTest.java
@@ -10,11 +10,6 @@ class PointTest {
     void testMoveRelativeIntParameter() {
         Point point = new Point(5, 5);
         point.moveRelative(5, 10);
-        /*assertEquals(10, point.getXValue());
-        assertEquals(15, point.getYValue());
-        assertAll("point",
-                () -> assertEquals(10, point.getXValue()),
-                () -> assertEquals(15, point.getYValue()));*/
         assertEquals(true, point.equals(new Point(10, 15)));
     }
 
@@ -22,8 +17,6 @@ class PointTest {
     void testMoveRelativePointParameter() {
         Point point = new Point(5, 5);
         point.moveRelative(new Point(5, 10));
-        /*assertEquals(10, point.getXValue());
-        assertEquals(15, point.getYValue());*/
         assertEquals(true, point.equals(new Point(10, 15)));
     }
 
@@ -31,8 +24,6 @@ class PointTest {
     void testMoveRelativePolarParameter() {
         Point point = new Point(5, 10);
         point.moveRelativePolar(10, 90.0);
-        /*assertEquals(5, point.getXValue());
-        assertEquals(20, point.getYValue());*/
         assertEquals(true, point.equals(new Point(5, 20)));
     }
 }
